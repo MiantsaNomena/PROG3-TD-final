8ce47bd25680d9551107c4f9297f6912e0b01743
Mandrindra <hei.mandrindra@gmail.com>
2026-05-06 15:39:29 +0300

    feat(dto): add CollectivityOverallStats DTO

--- src/main/java/com/example/fca/entity/dto/CollectivityOverallStats.java
+++ src/main/java/com/example/fca/entity/dto/CollectivityOverallStats.java
@@ -1,4 +1,14 @@
 package com.example.fca.entity.dto;
 
+import lombok.Data;
+
+@Data
 public class CollectivityOverallStats {
+    private String collectivityId;
+    private String collectivityName;
+    private String collectivityNumber;
+    private Integer newMembersNumber;
+    private Double overallMemberCurrentDuePercentage;
 }
+
+
