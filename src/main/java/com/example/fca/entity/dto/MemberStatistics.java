19b635938de746f892b26e7fc8a111a7d29cddbe
Mandrindra <hei.mandrindra@gmail.com>
2026-05-06 15:45:02 +0300

    feat(dto): add MemberStatistics DTO

--- src/main/java/com/example/fca/entity/dto/MemberStatistics.java
+++ src/main/java/com/example/fca/entity/dto/MemberStatistics.java
@@ -1,4 +1,14 @@
 package com.example.fca.entity.dto;
 
+import lombok.Data;
+
+@Data
 public class MemberStatistics {
+    private String memberId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String occupation;
+    private Double earnedAmount;
+    private Double unpaidAmount;
 }
