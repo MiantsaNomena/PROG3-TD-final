52f9faf411a6b77ac618a1fb3f34518a05ad6506
Mandrindra <hei.mandrindra@gmail.com>
2026-05-06 15:40:43 +0300

    feat(dto): add MemberDescription DTO

--- src/main/java/com/example/fca/entity/dto/MemberDescription.java
+++ src/main/java/com/example/fca/entity/dto/MemberDescription.java
@@ -1,4 +1,12 @@
 package com.example.fca.entity.dto;
 
+import lombok.Data;
+
+@Data
 public class MemberDescription {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String occupation;
 }
